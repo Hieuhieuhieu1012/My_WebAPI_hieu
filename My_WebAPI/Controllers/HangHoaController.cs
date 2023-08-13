8ae923a44bcb6e3e34a8b163de3d8ecd34d144d0
Hieuhieuhieu1012 <117344638+Hieuhieuhieu1012@users.noreply.github.com>
2023-08-14 00:38:18 +0700

    Add project files.

---
+++ My_WebAPI/Controllers/HangHoaController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_WebAPI.Model;
+
+namespace My_WebAPI.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public static List<Hanghoa> hanghoas= new List<Hanghoa>(); 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hanghoas);
+        }
+        [HttpPost]
+        public IActionResult TaoMoi(HanghoaVM hanghoaVM) 
+        {
+            var hangHoa = new Hanghoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hanghoaVM.TenHangHoa,
+                DonGia = hanghoaVM.DonGia
+
+            };
+            hanghoas.Add(hangHoa);
+            return Ok(
+                        new
+                        {
+                            Succeeded = true,
+                            Data = hangHoa
+                        }
+                     );
+        }
+       
+    }
+}
---
+++ My_WebAPI/Model/Hanghoa.cs
@@ -0,0 +1,13 @@
+﻿namespace My_WebAPI.Model
+{
+    public class HanghoaVM
+    {
+        public string TenHangHoa { get; set;}
+        public double DonGia { get; set;}
+    }
+    public class Hanghoa : HanghoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+        
+    }
+}
